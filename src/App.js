f908c421eb2ca682b8d2e577fc69899b6b30ea21
Jakob Krigovsky <jakob@krigovsky.com>
2023-02-08 15:42:26 +0100

    Add mock table

--- src/App.js
+++ src/App.js
@@ -1,4 +1,27 @@
+import React from 'react';
+import Table from 'react-bootstrap/Table';
+
 function App() {
+  return (
+    <Table>
+      <thead>
+        <tr>
+          <th>Postleitzahl</th>
+          <th>Gemeindeteil</th>
+          <th>Straße</th>
+          <th>Hausnummer</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>18147</td>
+          <td>Gehlsdorf</td>
+          <td>Blockweg</td>
+          <td>3</td>
+        </tr>
+      </tbody>
+    </Table>
+  );
 }
 
 export default App;
