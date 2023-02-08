ba19f553fe04e61d69d5a06697c7766a39cfdfbf
Jakob Krigovsky <jakob@krigovsky.com>
2023-02-08 15:54:01 +0100

    Use TanStack Table

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
 import React from 'react';
 import Table from 'react-bootstrap/Table';
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from '@tanstack/react-table';
+
+const columnHelper = createColumnHelper();
+
+const columns = [
+  columnHelper.accessor('postleitzahl', {
+    header: 'Postleitzahl',
+  }),
+  columnHelper.accessor('gemeindeteil_name', {
+    header: 'Gemeindeteil',
+  }),
+  columnHelper.accessor('strasse_name', {
+    header: 'Straße',
+  }),
+  columnHelper.accessor('hausnummer', {
+    header: 'Hausnummer',
+  }),
+];
+
+const data = [
+  {
+    postleitzahl: '18147',
+    // eslint-disable-next-line camelcase
+    gemeindeteil_name: 'Gehlsdorf',
+    // eslint-disable-next-line camelcase
+    strasse_name: 'Blockweg',
+    hausnummer: '3',
+  },
+];
 
 function App() {
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
   return (
     <Table>
       <thead>
-        <tr>
-          <th>Postleitzahl</th>
-          <th>Gemeindeteil</th>
-          <th>Straße</th>
-          <th>Hausnummer</th>
-        </tr>
+        {table.getHeaderGroups().map(headerGroup => (
+          <tr key={headerGroup.id}>
+            {headerGroup.headers.map(header => (
+              <th key={header.id}>
+                {flexRender(
+                  header.column.columnDef.header,
+                  header.getContext(),
+                )}
+              </th>
+            ))}
+          </tr>
+        ))}
       </thead>
       <tbody>
-        <tr>
-          <td>18147</td>
-          <td>Gehlsdorf</td>
-          <td>Blockweg</td>
-          <td>3</td>
-        </tr>
+        {table.getRowModel().rows.map(row => (
+          <tr key={row.id}>
+            {row.getVisibleCells().map(cell => (
+              <td key={cell.id}>
+                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+              </td>
+            ))}
+          </tr>
+        ))}
       </tbody>
     </Table>
   );
